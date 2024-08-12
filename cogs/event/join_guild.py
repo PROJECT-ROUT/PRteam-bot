c8bd787c38749f92a7bb587d26f789f727542be6
ch1enistonogii <roma228png@gmail.com>
2024-08-13 01:19:06 +0500

    выдача роли новичкам на сервере

--- cogs/event/join_guild.py
+++ cogs/event/join_guild.py
@@ -2,6 +2,8 @@ import discord
 from discord.ext import commands
 from cogs.commands.reg import Reg
 
+from main import config
+
 
 class Join_guild(commands.Cog):
     def __init__(self, client):
@@ -15,6 +17,12 @@ class Join_guild(commands.Cog):
     @commands.Cog.listener()
     async def on_member_join(self, member):
         await member.send(view=Join_guild.Batton(), content=f"Приветствую, {member.mention}! Я бот проекта Minecraft. Чтобы попасть на сервер - тебе необходимо зарегистрироваться. \n \n Напиши мне команду **/reg** и создай свой профиль в игре. Полный список моих команд ты можешь узнать, написав команду **/help**.")
+
+        author = member
+        guild = self.client.get_guild(config.bot.guild)
+        role = guild.get_role(config.bot.member_role)
+        await author.add_roles(role)
+
 
 async def setup(client):
     await client.add_cog(Join_guild(client))
